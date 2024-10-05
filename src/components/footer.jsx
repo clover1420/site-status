b3bad8fb6afbdae134f9a6af7f8cd41470654114
clover <748883120@qq.com>
2024-10-05 12:53:23 +0800

    魔改

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,42 +9,43 @@ const Footer = () => {
   const homeUrl = import.meta.env.VITE_HOME_URL;
   const emailUrl = import.meta.env.VITE_EMAIL_URL;
   const siteIcp = import.meta.env.VITE_SITE_ICP;
+  const Copyright =  import.meta.env.VITE_COPYRIGHT
 
   return (
     <footer id="footer">
-      <div className="social">
-        <CustomLink
-          iconDom={<GithubOne />}
-          to={`https://github.com/${githubName}/`}
-        />
-        <CustomLink iconDom={<Home />} to={homeUrl} />
-        <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
-      </div>
-      <div className="text">
-        <p>
+        <div className="social">
           <CustomLink
-            text={Package.alia}
-            to="https://github.com/imsyy/site-status"
+            iconDom={<GithubOne />}
+            to={`https://github.com/${githubName}/`}
           />
-          &nbsp;Version&nbsp;{Package.version}
-        </p>
-        <p>
-          基于&nbsp;
-          <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
-          &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
-        </p>
-        <p>
-          Copyright&nbsp;&copy;&nbsp;2020&nbsp;-&nbsp;{new Date().getFullYear()}
-          &nbsp;
-          <CustomLink to="https://www.imsyy.top/" text="無名" />
-          {siteIcp ? (
-            <React.Fragment>
-              &nbsp;|&nbsp;
-              <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
-            </React.Fragment>
-          ) : null}
-        </p>
-      </div>
+          <CustomLink iconDom={<Home />} to={homeUrl} />
+          <CustomLink iconDom={<Mail />} to={`mailto:${emailUrl}`} />
+        </div>
+        <div className="text">
+          <p>
+            <CustomLink
+              text={Package.alia}
+              to="https://github.com/imsyy/site-status"
+            />
+            &nbsp;Version&nbsp;{Package.version}
+          </p>
+          <p>
+            基于&nbsp;
+            <CustomLink to="https://uptimerobot.com/" text="UptimeRobot" />
+            &nbsp;接口&nbsp;|&nbsp;检测频率 5 分钟
+          </p>
+          <p>
+            Copyright&nbsp;&copy;&nbsp;2019&nbsp;-&nbsp;{new Date().getFullYear()}
+            &nbsp;
+            <CustomLink to="https://www.imsyy.top/" text={Copyright} />
+            {siteIcp ? (
+              <React.Fragment>
+                &nbsp;|&nbsp;
+                <CustomLink to="https://beian.miit.gov.cn/" text={siteIcp} />
+              </React.Fragment>
+            ) : null}
+          </p>
+        </div>
     </footer>
   );
 };
